c36bd7d192849562a084699425685a700dc6ceaa
guilhermealvesd <alvessig@gmail.com>
2024-05-23 20:25:27 -0300

    Versão 1.0.

---
+++ main.py
@@ -0,0 +1,12 @@
+#Lista
+
+nomes = ['Alex', 'Simone', 'Bernardo', 'César', 'Alexandra']
+
+#Ordenando a lista em ordem alfabética
+
+nomes.sort()
+
+#Existe a lista na tela
+
+for i in range(len(nomes)):
+    print(f'{i + 1}º nome da lista: {nomes[i]}.')
